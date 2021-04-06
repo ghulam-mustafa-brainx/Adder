452bbaf315a4392ea75014bd0746169fbcdcda5f
Ghulam Mustafa <ghulam.mustafa@brainxtech.com>
2021-04-06 20:10:36 +0500

    Initial Commit

---
+++ Adder/Adder/Adder.h
@@ -0,0 +1,18 @@
+//
+//  Adder.h
+//  Adder
+//
+//  Created by BrainX IOS 4 on 2021-04-06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Adder.
+FOUNDATION_EXPORT double AdderVersionNumber;
+
+//! Project version string for Adder.
+FOUNDATION_EXPORT const unsigned char AdderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Adder/PublicHeader.h>
+
+
---
+++ Adder/Adder/Adder.swift
@@ -0,0 +1,16 @@
+//
+//  Adder.swift
+//  Adder
+//
+//  Created by BrainX IOS 4 on 2021-04-06.
+//
+
+import Foundation
+import UIKit
+
+open class Adder {
+    
+    open class func add(value1: Int, value2: Int) -> Int {
+        return value1 + value2
+    }
+}
